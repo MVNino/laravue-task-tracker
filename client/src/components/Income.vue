725a30afe64c92613f9cc461a7ea5675d4078e98
marlon nino <mvnino.halcyondigital@gmail.com>
2024-04-13 15:26:21 +0800

    move vue app to client folder

---
+++ client/src/components/Income.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h4>Income</h4>
+        <p id="money-plus" class="money plus">${{ income }}</p>
+    </div>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+
+const props = defineProps({
+    income: {
+        type: Number,
+        required: true
+    }
+})
+</script>
